2064fbf68d7b9f038823618499db571ca1045dec
PS-Soundwave <PS-Soundwave@users.noreply.github.com>
2023-10-25 00:46:17 +0000

    Move PasswordUtil to Util

--- src/main/java/Util/PasswordUtil.java
+++ src/main/java/Util/PasswordUtil.java
@@ -1,9 +1,10 @@
+package Util;
 public class PasswordUtil {
+    // CM: Would prefer this on Model.Account but placed it here given the instructions
     /*
      * Returns true if and only if the password is valid for a new account.
      */
     public static boolean isValid(String password) {
         return password.length() >= 4;
     }
-    // CM: Would prefer this on Model.Account but placed it here given the instructions
 }
