2274c7cb2f561cb2619e98b79981d79732d273c7
PS-Soundwave <PS-Soundwave@users.noreply.github.com>
2023-10-25 00:47:07 +0000

    Add messages endpoint

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,12 +1,15 @@
 package Controller;
 
 import Model.Account;
+import Model.Message;
 import Service.AccountService;
+import Service.MessageService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 public class SocialMediaController {
-    AccountService service = new AccountService();
+    AccountService accountService = new AccountService();
+    MessageService messageService = new MessageService();
 
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
@@ -17,7 +20,10 @@ public class SocialMediaController {
         Javalin app = Javalin.create();
 
         app.post("/register", this::postRegister);
+
         app.post("/login", this::postLogin);
+
+        app.post("/messages", this::postMessages);
 
         return app;
     }
@@ -25,7 +31,7 @@ public class SocialMediaController {
     private void postRegister(Context ctx) {
         Account account = ctx.bodyAsClass(Account.class);
 
-        account = service.createAccount(account.username, account.password);
+        account = accountService.createAccount(account.username, account.password);
 
         if (account == null) {
             ctx.status(400);
@@ -38,13 +44,29 @@ public class SocialMediaController {
     private void postLogin(Context ctx) {
         Account account = ctx.bodyAsClass(Account.class);
 
-        account = service.validateLogin(account.getUsername(), account.getPassword());
+        account = accountService.validateLogin(account.getUsername(), account.getPassword());
 
         if (account == null) {
             ctx.status(401);
         }
         else {
             ctx.json(account);
+        }
+    }
+
+    private void postMessages(Context ctx) {
+        Message message = ctx.bodyAsClass(Message.class);
+
+        /* CM: *Really* no reason to be trusting the message is posted with a correct timestamp.
+         * Just done in case tests expect it to be respected.
+         */
+        message = messageService.postMessage(message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+
+        if (message == null) {
+            ctx.status(400);
+        }
+        else {
+            ctx.json(message);
         }
     }
 }
